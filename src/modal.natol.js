16dcb33c41a9c02df44324a3f81ae6f0f9d8ea81
raulanatol <raulanatol@deportunity.com>
2015-03-12 11:27:44 +0000

    Adaptation for basic document load

--- src/modal.natol.js
+++ src/modal.natol.js
@@ -8,26 +8,32 @@
  * http://www.opensource.org/licenses/mit-license.php
  *
  */
-(function ($) {
-    $('.ntl-modal').each(function (i, element) {
-        var $element = $(element);
-        var url = $element.attr('href');
-        var modalContent = $element.attr('ntl-content');
 
-        $element.click(function (event) {
-            event.preventDefault();
-            var containerId = modalContent + '-container';
-            var container = $('#' + containerId);
-            if (!container.length) {
-                $('body').append('<div id="' + containerId + '"></div>');
-                $('#' + containerId).load(url, function () {
-                    $('#' + modalContent).modal('toggle');
+/*JS Lint helpers: */
+/*global $, jQuery, window, document */
+(function ($, document) {
+    $(document).ready(function () {
+            $('.ntl-modal').each(function (i, element) {
+                var $element = $(element);
+                var url = $element.attr('href');
+                var modalContent = $element.attr('ntl-content');
+
+                $element.click(function (event) {
+                    event.preventDefault();
+                    var containerId = modalContent + '-container';
+                    var container = $('#' + containerId);
+                    if (!container.length) {
+                        $('body').append('<div id="' + containerId + '"></div>');
+                        $('#' + containerId).load(url, function () {
+                            $('#' + modalContent).modal('toggle');
+                        });
+                    } else {
+                        $('#' + modalContent).modal('toggle');
+                    }
                 });
-            } else {
-                $('#' + modalContent).modal('toggle');
-            }
-        });
-    });
+            });
+        }
+    );
 
     return this;
-})(jQuery);
+})(jQuery, document);
